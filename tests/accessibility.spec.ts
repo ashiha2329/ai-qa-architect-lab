2c666395e4f961f3d944146d69fdee2cf48b86ef
ashiha2329 <ashiha@optonline.net>
2026-09-04 16:43:46 -0400

    Attach Axe accessibility results to test reports

--- tests/accessibility.spec.ts
+++ tests/accessibility.spec.ts
@@ -4,12 +4,25 @@ import AxeBuilder from '@axe-core/playwright';
 test.describe('Accessibility', () => {
   test('login page has no automatically detectable accessibility violations', async ({
     page,
-  }) => {
+  }, testInfo) => {
     await page.goto('/');
 
     const accessibilityScanResults = await new AxeBuilder({ page })
       .withTags(['wcag2a', 'wcag2aa'])
       .analyze();
+
+    await testInfo.attach('Axe WCAG 2 A and AA scan results', {
+      body: JSON.stringify(
+        {
+          scannedUrl: accessibilityScanResults.url,
+          violationsFound: accessibilityScanResults.violations.length,
+          violations: accessibilityScanResults.violations,
+        },
+        null,
+        2,
+      ),
+      contentType: 'application/json',
+    });
 
     expect(accessibilityScanResults.violations).toEqual([]);
   });
